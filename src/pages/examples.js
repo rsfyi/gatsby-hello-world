2eb831d8833eb82e3b2a12f3a609ca12c73fa0f0
Rahul Singh <rscodedocs@gmail.com>
2020-06-13 10:59:54 +0530

    graphql aliases and query

---
+++ src/pages/examples.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Header from "../examples/Header"
+import Layout from "../components/Layout"
+
+const examples = () => {
+  return (
+    <Layout>
+      <h1>Hello from examples page</h1>
+      <Header />
+    </Layout>
+  )
+}
+
+export default examples
