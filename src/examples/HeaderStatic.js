e8b24aff1e6814dbfb6e0521bf9b5b79e8d82031
Rahul Singh <rscodedocs@gmail.com>
2020-06-13 11:31:54 +0530

    finished query types

---
+++ src/examples/HeaderStatic.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+
+const ComponentName = () => (
+  <StaticQuery
+    query={graphql`
+      {
+        site {
+          info: siteMetadata {
+            author
+            data
+            title
+            description
+            person {
+              age
+              name
+            }
+          }
+        }
+      }
+    `}
+    render={data => <pre>{JSON.stringify(data, null, 4)}</pre>}
+  ></StaticQuery>
+)
+
+export default ComponentName
--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,13 +1,33 @@
 import React from "react"
-
+import { graphql } from "gatsby"
 import Header from "../examples/Header"
+import StaticHeader from "../examples/HeaderStatic"
 import Layout from "../components/Layout"
 
-const examples = () => {
+export const data = graphql`
+  {
+    site {
+      info: siteMetadata {
+        author
+        data
+        title
+        description
+        person {
+          age
+          name
+        }
+      }
+    }
+  }
+`
+
+const examples = props => {
   return (
     <Layout>
       <h1>Hello from examples page</h1>
       <Header />
+      <StaticHeader />
+      <p>PageQuery - Author : {props.data.site.info.author}</p>
     </Layout>
   )
 }
