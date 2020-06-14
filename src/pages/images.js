1abfcd6986de191daf7ec433be80aae064b26cb6
Rahul Singh <rscodedocs@gmail.com>
2020-06-14 17:50:39 +0530

    finished image optimizations

---
+++ src/pages/images.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+
+import Images from "../examples/Images"
+
+export default function images() {
+  return (
+    <Layout>
+      <Images />
+    </Layout>
+  )
+}
